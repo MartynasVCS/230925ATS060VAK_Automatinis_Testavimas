cb512c17768fee39fdb27bb9c88d4dde2bdc4a98
MartynoSiela <m.satas@gmail.com>
2023-10-25 19:11:19 +0300

    Add calculator app code

---
+++ Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 5;
+            int number2 = 2;
+
+            if (Calculator.Multiply(number1, number2) == 10)
+            {
+                Console.WriteLine($"Multiplication of {number1} and {number2} works as expected");
+            }
+            else
+            {
+                Console.WriteLine($"Error when multiplying {number1} and {number2}!");
+            }
+
+            if (Calculator.Multiply(7, 15) == 105)
+            {
+                Console.WriteLine($"Multiplication of {7} and {15} works as expected");
+            }
+            else
+            {
+                Console.WriteLine($"Error when multiplying {7} and {15}!");
+            }
+
+            if (Calculator.Divide(number1, number2) == 2.5)
+            {
+                Console.WriteLine($"Multiplication of {number1} and {number2} works as expected");
+            }
+            else
+            {
+                Console.WriteLine($"Error when dividing {number1} and {number2}!");
+            }
+        }
+    }
+}
