a0c418d5aa8f9bc8843b2fbf88da9fcfbb9b2fe7
MartynasVCS <martynasvcs@gmail.com>
2023-10-25 20:15:06 +0300

    Add example of using NUnit TestCase annotation

--- CalculatorUnitTests/Tests.cs
+++ CalculatorUnitTests/Tests.cs
@@ -20,13 +20,12 @@ namespace CalculatorUnitTests
             Assert.AreEqual(expectedResult, actualResult);
         }
 
-        [Test]
-        public void TestDivision()
+        [TestCase(5, 2, 2.5)]
+        [TestCase(7, 2, 3.5)]
+        [TestCase(1, 3, 0.3333)]
+        [TestCase(1, 8, 0.125)]
+        public void TestDivision(int number1, int number2, double expectedResult)
         {
-            int number1 = 5;
-            int number2 = 2;
-            double expectedResult = 2.5;
-
             double actualResult = Methods.Divide(number1, number2);
 
             Assert.AreEqual(expectedResult, actualResult);
