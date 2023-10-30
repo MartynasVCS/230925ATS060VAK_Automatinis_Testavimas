b247b0fff59093aba0ac4d414376559e9d1cc432
MartynasVCS <martynasvcs@gmail.com>
2023-10-30 23:34:36 +0200

    Add demo test for Selenium Easy website

---
+++ SeleniumTestsWithoutPOM/SeleniumEasyTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestsWithoutPOM
+{
+    internal class SeleniumEasyTests
+    {
+        [Test]
+        public void SingleInputFieldTest()
+        {
+            string expectedMessage = "gshdujakfgksudjh";
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://demo.seleniumeasy.com/basic-first-form-demo.html";
+
+            var inputEnterMessage = driver.FindElement(By.XPath("//*[@id='get-input']//input[@id='user-message']"));
+            inputEnterMessage.SendKeys(expectedMessage);
+
+            var buttonShowMessage = driver.FindElement(By.XPath("//*[@id='get-input']/button"));
+            buttonShowMessage.Click();
+
+            var spanMessage = driver.FindElement(By.XPath("//*[@id='get-input']/following-sibling::*/span"));
+            string actualResult = spanMessage.Text;
+
+            driver.Quit();
+
+            Assert.AreEqual(expectedMessage, actualResult);
+        }
+    }
+}
