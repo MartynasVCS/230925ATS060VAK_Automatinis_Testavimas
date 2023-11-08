dbb1ee38e13fcde6c150cb03fd5919896a7e18a8
MartynasVCS <martynasvcs@gmail.com>
2023-11-08 20:36:03 +0200

    refactor SeleniumEasy test to use POM

---
+++ SeleniumFramework/Pages/SeleniumEasy/BasicFirstFormDemo.cs
@@ -0,0 +1,28 @@
+﻿namespace SeleniumFramework.Pages.SeleniumEasy
+{
+    public class BasicFirstFormDemo
+    {
+        public static void Open()
+        {
+            Driver.OpenPage("https://demo.seleniumeasy.com/basic-first-form-demo.html");
+        }
+
+        public static void EnterMessage(string message)
+        {
+            string locator = "//*[@id='get-input']//input[@id='user-message']";
+            Common.SendKeysToElement(locator, message);
+        }
+
+        public static void ClickButtonShowMessage()
+        {
+            string locator = "//*[@id='get-input']/button";
+            Common.ClickElement(locator);
+        }
+
+        public static string GetMessage()
+        {
+            string locator = "//*[@id='get-input']/following-sibling::*/span";
+            return Common.GetElementText(locator);
+        }
+    }
+}
---
+++ SeleniumTests/SeleniumEasy/BasicFirstFormDemoTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using SeleniumFramework.Pages.SeleniumEasy;
+using SeleniumTests.BaseTests;
+
+namespace SeleniumTests.SeleniumEasy
+{
+    internal class BasicFirstFormDemoTests : BaseTest
+    {
+        [Test]
+        public void SingleInputField()
+        {
+            string expectedMessage = "Hellow World";
+
+            BasicFirstFormDemo.Open();
+            BasicFirstFormDemo.EnterMessage(expectedMessage);
+            BasicFirstFormDemo.ClickButtonShowMessage();
+            string actualMessage = BasicFirstFormDemo.GetMessage();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+    }
+}
